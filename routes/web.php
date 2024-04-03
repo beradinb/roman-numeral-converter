25ec6854fc4240470b0b09440c308c928d7aefef
Beradin Bicaku <beradin.bicaku@pdm-ltd.com>
2024-04-03 13:22:17 +0100

    setting up controller, view and route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ConverterController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/convert', [ConverterController::class, 'convert']);
